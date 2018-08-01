108a7de8b64d0a1a505b7f3ddb599c8dfeff8b36
DavidDG <david.digloria@hotmail.com>
2018-08-01 14:42:49 +0200

    commit 1

---
+++ MoumouteProject/moumouteProject/src/main/java/com/spring/henallux/moumouteProject/MoumouteProjectApplication.java
@@ -0,0 +1,12 @@
+package com.spring.henallux.moumouteProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoumouteProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoumouteProjectApplication.class, args);
+	}
+}
---
+++ MoumouteProject/moumouteProject/src/test/java/com/spring/henallux/moumouteProject/MoumouteProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spring.henallux.moumouteProject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MoumouteProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
