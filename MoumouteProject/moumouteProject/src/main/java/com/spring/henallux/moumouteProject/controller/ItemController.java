49139a4b4030717b130412da803e98ebc626f9f7
DavidDG <david.digloria@hotmail.com>
2018-08-19 15:51:38 +0200

    commit

--- MoumouteProject/moumouteProject/src/main/java/com/spring/henallux/moumouteProject/controller/ItemController.java
+++ MoumouteProject/moumouteProject/src/main/java/com/spring/henallux/moumouteProject/controller/ItemController.java
@@ -1,19 +1,48 @@
 package com.spring.henallux.moumouteProject.controller;
 
+import com.spring.henallux.moumouteProject.dataAccess.dao.WigDAO;
+import com.spring.henallux.moumouteProject.model.CartItem;
+import com.spring.henallux.moumouteProject.model.Constants;
+import com.spring.henallux.moumouteProject.model.SearchWigForm;
 import com.spring.henallux.moumouteProject.model.Wig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+import java.util.Locale;
 
 @Controller
 @RequestMapping(value="/items")
 public class ItemController {
 
-    @RequestMapping(value="/search", method = RequestMethod.POST)
-    public String getFormData(@ModelAttribute(value="searchItems")Wig wig)
+    private WigDAO wigDAO;
+
+    @Autowired
+    ItemController(WigDAO wigDao)
     {
-        return "integrated:home";
+        this.wigDAO = wigDao;
+    }
+
+    @ModelAttribute(Constants.CART)
+    public HashMap<Integer,CartItem> cart(){
+        return new HashMap<>();
+    }
+
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String home(Model model, @ModelAttribute(value = Constants.CART)HashMap<Long, CartItem> cart, @RequestParam(value="itemId")int idItem, Locale locale)
+    {
+        System.out.println("VAL" + idItem);
+        model.addAttribute("itemToSearch", new SearchWigForm());
+        model.addAttribute("title","");
+        model.addAttribute("cartSize", cart.size());
+        model.addAttribute("item",wigDAO.getWigFromId(idItem,"FR"));
+        return "integrated:itemDetail";
     }
 
 
--- MoumouteProject/moumouteProject/src/main/java/com/spring/henallux/moumouteProject/model/CartItem.java
+++ MoumouteProject/moumouteProject/src/main/java/com/spring/henallux/moumouteProject/model/CartItem.java
@@ -3,5 +3,69 @@ package com.spring.henallux.moumouteProject.model;
 
 public class CartItem
 {
+    public CartItem(int numLine, double EVATPrice, double VATRate, int quantity, String pictureLink, String wigName) {
+        this.numLine = numLine;
+        this.EVATPrice = EVATPrice;
+        this.VATRate = VATRate;
+        this.quantity = quantity;
+        this.pictureLink = pictureLink;
+        this.wigName = wigName;
+    }
 
+    public int getNumLine() {
+        return numLine;
+    }
+
+    public void setNumLine(int numLine) {
+        this.numLine = numLine;
+    }
+
+    private int numLine;
+    private double EVATPrice;
+    private double VATRate;
+    private int quantity;
+    private String pictureLink;
+    private String wigName;
+
+
+
+    public double getEVATPrice() {
+        return EVATPrice;
+    }
+
+    public void setEVATPrice(double EVATPrice) {
+        this.EVATPrice = EVATPrice;
+    }
+
+    public double getVATRate() {
+        return VATRate;
+    }
+
+    public void setVATRate(double VATRate) {
+        this.VATRate = VATRate;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getPictureLink() {
+        return pictureLink;
+    }
+
+    public void setPictureLink(String pictureLink) {
+        this.pictureLink = pictureLink;
+    }
+
+    public String getWigName() {
+        return wigName;
+    }
+
+    public void setWigName(String wigName) {
+        this.wigName = wigName;
+    }
 }
